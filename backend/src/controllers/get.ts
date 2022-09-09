c88fbbbd3b400b497891ba6fafb433731e70da32
WolfieLeader <koren2318@gmail.com>
2022-09-09 15:19:24 +0300

    Get commands are looking good

--- backend/src/controllers/get.ts
+++ backend/src/controllers/get.ts
@@ -1,31 +1,24 @@
 import { Request, Response, NextFunction } from "express";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import connectionSettings from "../helpers/defaultSettings";
 import { convertErrorToString, convertParamsToInt } from "../helpers";
 
+const getHelper = async (res: Response, sql: string) => {
+  try {
+    const connection = await mysql.createConnection(connectionSettings);
+    const [rows] = await connection.execute(sql);
+    res.status(200).json(rows);
+    await connection.end();
+  } catch (error) {
+    res.status(500).json({ error: convertErrorToString(error) });
+  }
+};
 export const getUsers = (req: Request, res: Response) => {
-  const connection = mysql.createConnection(connectionSettings);
-  connection.query("SELECT * FROM users", (error, results) => {
-    if (error) {
-      return res.status(500).json({ error });
-    }
-    return res.status(200).json({ results });
-  });
-  connection.end();
+  getHelper(res, "SELECT * FROM users");
 };
 
-export const getUsersById = (req: Request, res: Response) => {
-  const connection = mysql.createConnection(connectionSettings);
-  const { id } = req.params;
-  const ids = convertParamsToInt(id);
-  if (ids === null) {
-    return res.status(400).json({ error: "Invalid id" });
-  }
-  connection.query(`SELECT * FROM users WHERE id IN (${ids.join(",")})`, (error, results) => {
-    if (error) {
-      return res.status(500).json({ error });
-    }
-    return res.status(200).json({ results });
-  });
-  connection.end();
+export const getUsersById = async (req: Request, res: Response) => {
+  const ids = convertParamsToInt(req.params.id);
+  if (ids === null) return res.status(400).json({ error: "Invalid id" });
+  getHelper(res, `SELECT * FROM users WHERE id IN (${ids.join(",")});`);
 };
